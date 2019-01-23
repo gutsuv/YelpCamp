19bb34ec125de5c853a68ad994ccfaa754488e06
Viktor Gutsu <victorgutsu@gmail.com>
2019-01-23 09:07:33 +0000

    extended flash message

--- middleware/index.js
+++ middleware/index.js
@@ -6,7 +6,7 @@ middlewareObj.isLoggedIn = function(req, res, next) {
     if(req.isAuthenticated()) {
         return next();
     }
-    req.flash("error", "You need to be logged in to do that!");
+    req.flash("error", "You need to be logged in to do that!\nFeel free to sign up!");
     res.redirect("/login");
 }
 
