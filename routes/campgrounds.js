6469038b11423aae7e76929bbd54e7e5703c76f6
Viktor Gutsu <victorgutsu@gmail.com>
2019-01-23 09:00:58 +0000

    styling

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -83,7 +83,7 @@ router.put("/campgrounds/:id", middleware.checkCampgroundOwnership, function(req
 
 // Destroy Campground
 router.delete("/campgrounds/:id", middleware.checkCampgroundOwnership, function(req, res) {
-    Campground.findOneAndDelete(req.params.id, function(err) {
+    Campground.findByIdAndRemove(req.params.id, function(err) {
         if(err) {
             res.redirect("/campgrounds");
         } else {
